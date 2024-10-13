7bb4e8c9139f61370d971092973d5d58159e54bf
Yae Miko <164617880+Miko756@users.noreply.github.com>
2024-10-13 21:54:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Free")
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DB_URI)
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002206999262"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002445549316"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002280973372"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "20"))
